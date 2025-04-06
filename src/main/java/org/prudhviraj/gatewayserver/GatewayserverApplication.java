1d53ed7f233bfa7362e1d4b845e664f32aa1d59b
Bavigadda <prudhviraj729@gmail.com>
2025-04-07 00:11:26 +0530

    Implemented custom route configurations for Save Money Bank Gateway - Added routes for accounts, loans, and cards with URI mappings - Configured path rewrite filters for dynamic routing - Added custom response headers to include X-Response-Time

--- src/main/java/org/prudhviraj/gatewayserver/GatewayserverApplication.java
+++ src/main/java/org/prudhviraj/gatewayserver/GatewayserverApplication.java
@@ -2,12 +2,40 @@ package org.prudhviraj.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class GatewayserverApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayserverApplication.class, args);
+    }
+
+
+    @Bean
+    public RouteLocator saveMoneyBankRouteLocatorConfig(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder.routes()
+                .route(p -> p
+                        .path("/savemoneybank/accounts/**")
+                        .filters( f -> f.rewritePath("/savemoneybank/accounts/(?<segment>.*)","/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+                        .uri("lb://ACCOUNTS"))
+                .route(p -> p
+                        .path("/savemoneybank/loans/**")
+                        .filters( f -> f.rewritePath("/savemoneybank/loans/(?<segment>.*)","/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+                        .uri("lb://LOANS"))
+                .route(p -> p
+                        .path("/savemoneybank/cards/**")
+                        .filters( f -> f.rewritePath("/savemoneybank/cards/(?<segment>.*)","/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+                        .uri("lb://CARDS")).build();
+
+
     }
 
 }
