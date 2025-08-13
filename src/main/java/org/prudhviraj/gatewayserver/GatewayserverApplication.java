8cd4560f45a7b26e98dd98278e8ce7bd1a653d48
Bavigadda_prudhviraj <prudhviraj729@gmail.com>
2025-08-13 11:39:41 +0530

    Enhance gateway server with correlation ID handling and Resilience4j circuit breaker
    - Added correlation ID check and addition in ResponseTraceFilter.postGlobalFilter
    - Included logging for correlation ID updates in outbound headers
    - Configured ReactiveResilience4JCircuitBreakerFactory in gateway main class
    - Set default circuit breaker and 4-second timeout in TimeLimiterConfig
    - Added comments to application.properties for clarity

--- src/main/java/org/prudhviraj/gatewayserver/GatewayserverApplication.java
+++ src/main/java/org/prudhviraj/gatewayserver/GatewayserverApplication.java
@@ -1,7 +1,13 @@
 package org.prudhviraj.gatewayserver;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreaker;
+import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -69,6 +75,13 @@ public class GatewayserverApplication {
 
                 // Build the complete route configuration
                 .build();
+    }
+
+    @Bean
+    public Customizer<ReactiveResilience4JCircuitBreakerFactory> customizer() {
+        return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+                .circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
+                .timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(4)).build()).build());
     }
 
 
